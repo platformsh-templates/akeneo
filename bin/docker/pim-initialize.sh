d667240653673d10a1ad5333698f5d457db704bb
Larry Garfield <larry@garfieldtech.com>
2019-09-27 09:55:42 -0500

    Update to latest upstream

---
+++ bin/docker/pim-initialize.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker-compose exec fpm rm -rf var/cache/*
+
+docker-compose exec fpm bin/console --env=prod pim:install --force --symlink --clean
+
+docker-compose run --rm node yarn run less
+docker-compose run --rm node yarn run webpack-dev
